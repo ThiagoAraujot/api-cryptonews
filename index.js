8b6360ae1ff53cf9cdddd2c935334f5f947f2641
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-02 13:04:32 -0300

    refactor: swagger route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import newsRoute from "./src/routes/news.route.js";
-import swaggerRoute from "./src/routes/swagger.route.js";
+import swaggerRoute from "./src/routes/swagger.route.cjs";
 
 dotenv.config();
 
