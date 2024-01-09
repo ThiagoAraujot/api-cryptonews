b70c15125428ae659619c68add375cf563bc26ec
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-09 13:08:38 -0300

    refactor: access control

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 connectcDataBase();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
