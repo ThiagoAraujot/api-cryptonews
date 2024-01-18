305bdb11205ad497ff84a07295806306bc68d1c4
thiago araujo <devthiagoaraujo@gmail.com>
2024-01-18 13:31:04 -0300

    fix: create user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ import { validId, validUser } from "../middlewares/global.middlewares.js";
 
 const route = Router();
 
-route.post("/", userController.create);
+route.post("/create", userController.create);
 route.get("/", userController.findAll);
 route.get("/:id", validId, validUser, userController.findById);
 route.patch("/:id", validId, validUser, userController.update);
